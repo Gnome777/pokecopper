1a2c9ef76c5209bd02c415f778d7e91f584cca82
Gnome67 <102388813+Gnome777@users.noreply.github.com>
2025-06-22 19:59:18 -0500

    Update bargain shop prices and Buena prize items
    Adjusted the prices of items in the bargain shop to lower values. Replaced and reordered Buena Prize items with a new set and updated their corresponding point costs.

--- data/items/buena_prizes.asm
+++ data/items/buena_prizes.asm
@@ -1,12 +1,12 @@
 BuenaPrizeItems:
 	table_width 2
-	db ULTRA_BALL,   2
-	db FULL_RESTORE, 2
-	db NUGGET,       3
-	db RARE_CANDY,   3
-	db PROTEIN,      5
-	db IRON,         5
-	db CARBOS,       5
-	db CALCIUM,      5
-	db HP_UP,        5
+	db POKE_BALL,    1
+	db POTION,       2
+	db GREAT_BALL,   3
+	db SUPER_POTION, 4
+	db ULTRA_BALL,   5
+	db HYPER_POTION, 6
+	db FULL_HEAL,    7
+	db FULL_RESTORE, 8
+	db RARE_CANDY,   9
 	assert_table_length NUM_BUENA_PRIZES
